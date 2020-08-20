47e64816a8726b943991bf6c54bc33c3bbae1959
Trey Jennings <tjennings343@gmail.com>
2020-08-20 01:23:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else phrase == phrase.upcase
-    return "NO, NOT SINCE 1938"
+    return "NO, NOT SINCE 1938!"
   end
 end
 
